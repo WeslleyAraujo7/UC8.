80757e6251b3637b3dbea71a77cbf766642bf7e1
Mateus Irineu <irineu.mateus11@gmail.com>
2025-07-28 20:56:48 -0300

    cubo

---
+++ cubo/app/index.jsx
@@ -0,0 +1,28 @@
+import { View, Text, StyleSheet } from 'react-native'
+import DraggbleSquare from '../components/DraggbleSquare'
+
+export default function HomeScreen() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Arraste o Quadrado</Text>
+            <DraggbleSquare/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff'
+    },
+
+    title: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    }
+
+
+})
