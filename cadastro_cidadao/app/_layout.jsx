83558c3c8d87b563272af34bfc91144e29c4908c
WeslleyAraujo <weslleyaraujo2025.1@gmail.com>
2025-10-02 23:58:57 +0000

    subiu

--- cadastro_cidadao/app/_layout.jsx
+++ cadastro_cidadao/app/_layout.jsx
@@ -1,24 +1,46 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import React, { useEffect } from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import HomeScreen from "./screens/HomeScreen";
+import CadastroScreen from "./screens/CadastroScreen";
+import HistoryScreen from "./screens/HistoryScreen";
+import ConfigsScreen from "./screens/ConfigsScreen";
+import AboutScreen from "./screens/AboutScreen";
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { initDB } from "../db/db";
 
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { COLORS } from "../styles/colors";
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  useEffect(() => {
+    initDB().catch((err) => {
+      console.log("DB init error:", err);
+    });
+  }, []);
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider>
+        <Drawer.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: COLORS.pale,
+            },
+            headerTintColor: COLORS.darkText,
+          }}
+        >
+            <Drawer.Screen name="Home" component={HomeScreen}/>
+            <Drawer.Screen name="Cadastro" component={CadastroScreen}/>
+            <Drawer.Screen name="Histórico" component={HistoryScreen}/>
+            <Drawer.Screen name="Configurações" component={ConfigsScreen}/>
+            <Drawer.Screen name="Sobre" component={AbScreen}/>
+        </Drawer.Navigator>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
